bd65efeb49f871382e747b392625c9b77a76e262
Jesus Vargas <jesusvarguer18@gmail.com>
2022-12-30 18:32:16 -0500

    Completando y eliminando TODOs 2.0

--- src/App.js
+++ src/App.js
@@ -8,47 +8,43 @@ import { CreateTodoButton } from './CreateTodoButton';
 
 const defaultTodos = [
   { text: 'Cortar cebolla', completed: true },
-  { text: 'Tomar el curso intro a React', completed: false },
-  { text: 'Hacer el almuerzo', completed: true },
-  { text: 'Tomar 7 vasos de agua', completed: false },
+  { text: 'Tomar el cursso de intro a React', completed: false },
+  { text: 'Llorar con la llorona', completed: true },
+  { text: 'LALALALAA', completed: false },
 ];
 
 function App() {
-
   const [todos, setTodos] = React.useState(defaultTodos);
-  // El estado de nuestra busqueda
   const [searchValue, setSearchValue] = React.useState('');
 
   const completedTodos = todos.filter(todo => !!todo.completed).length;
   const totalTodos = todos.length;
-  // Cree una nueva variable en donde guarde las conincidencias con la busqueda
+
   let searchedTodos = [];
 
-  // Lógica para filtrar la busqueda de los TODOs
   if (!searchValue.length >= 1) {
-    searchedTodos = defaultTodos
+    searchedTodos = todos;
   } else {
     searchedTodos = todos.filter(todo => {
       const todoText = todo.text.toLowerCase();
       const searchText = searchValue.toLowerCase();
       return todoText.includes(searchText);
-    })
+    });
   }
 
   const completeTodo = (text) => {
     const todoIndex = todos.findIndex(todo => todo.text === text);
-
     const newTodos = [...todos];
     newTodos[todoIndex].completed = true;
     setTodos(newTodos);
   };
 
   const deleteTodo = (text) => {
-    const todoIndex = todos.findIndex(todo => todo.text !== text);
-		const newTodos = [...todos];
-    newTodos.splice(todoIndex, 0);
+    const todoIndex = todos.findIndex(todo => todo.text === text);
+    const newTodos = [...todos];
+    newTodos.splice(todoIndex, 1);
     setTodos(newTodos);
-	};
+  };
 
   return (
     <React.Fragment>
