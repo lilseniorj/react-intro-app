fd0470d7ed4206903b2dbd25d5f6fd0aa1cf5700
Jesus Vargas <jesusvarguer18@gmail.com>
2023-01-06 02:15:59 -0500

    Deploy 1.1

--- src/TodoCounter/index.js
+++ src/TodoCounter/index.js
@@ -6,7 +6,7 @@ function TodoCounter() {
   const { totalTodos, completedTodos } = React.useContext(TodoContext);
 
   return (
-    <h2 className="TodoCounter">Haz completado {completedTodos} de {totalTodos} tareas</h2>
+    <h2 className="TodoCounter">Haz completado {completedTodos} de {totalTodos} tareas </h2>
   )
 }
 
